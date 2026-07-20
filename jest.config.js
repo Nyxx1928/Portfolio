0de0874ec0292f823d70a1457943f0b167bc5d54
Nyxx <nyxxlumapak@gmail.com>
2026-07-20 19:43:29 +0800

    chore(jest): remove outdated comment from config
    - Remove unnecessary explanatory comment about createJestConfig export
    - Simplify configuration file by removing redundant documentation

--- jest.config.js
+++ jest.config.js
@@ -33,5 +33,4 @@ const customJestConfig = {
   testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/', '<rootDir>/e2e/'],
 }
 
-// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
 module.exports = createJestConfig(customJestConfig)
